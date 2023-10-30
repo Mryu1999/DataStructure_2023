a2e50971d580db410a48260bc7c79bf3ebae97cb
Mryu <3338130157@qq.com>
2023-10-30 17:29:28 +0800

    最长公共子序列(迭代)

--- src/I/LongCommonSubsequence.java
+++ src/I/LongCommonSubsequence.java
@@ -1,6 +1,5 @@
 package I;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,29 +16,45 @@ public class LongCommonSubsequence {
         String b = "algorithm";
         List<Character> list1 = a.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
         List<Character> list2 = b.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
-        int result = recursiveLongCommonSubseq(list1, list1.size(), list2, list2.size());
-        System.out.println(result);
+        // 递归
+        System.out.println(recursiveLongCommonSubseq(list1, list1.size(), list2, list2.size()));
         // 递归+制表备查
         memo = new int[list1.size() + 1][list2.size() + 1];
         for (int[] row : memo) {
             Arrays.fill(row, -1);
         }
-        int resultByMemo = recursiveLSCByMemo(list1, list1.size(), list2, list2.size());
-        for (int[] row : memo) {
-            for (int i = 0; i < row.length; i++) {
-                System.out.print(row[i] == -1 ? "-1 " : " " + row[i] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println(resultByMemo);
+        System.out.println(recursiveLSCByMemo(list1, list1.size(), list2, list2.size()));
+        print2DArray(memo);
         // 动态规划
-        int resultByDynamic = lscDynamic(list1, list2);
+        System.out.println(lscDynamic(list1, list2));
     }
 
-    private static int[][] memo;// 备忘录，消除重复子问题
+    /**
+     * 递归实现
+     * 主要依靠减而治之和分而治之
+     * 时间复杂度：当两父序列长度相等时 O(2^n)
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    private static int recursiveLongCommonSubseq(List<Character> a, int aSize, List<Character> b, int bSize) {
+        if (aSize < 1 || bSize < 1) {
+            return 0;
+        }
+        if (a.get(aSize - 1).equals(b.get(bSize - 1))) {
+            return 1 + recursiveLongCommonSubseq(a, aSize - 1, b, bSize - 1);
+        } else {
+            return Math.max(recursiveLongCommonSubseq(a, aSize - 1, b, bSize), recursiveLongCommonSubseq(a, aSize, b, bSize - 1));
+        }
+    }
+
+    // 备忘录，消除重复子问题
+    private static int[][] memo;
 
     /**
      * 在递归的基础上引入备忘录，以解决重复子问题
+     *
      * @param a
      * @param aSize
      * @param b
@@ -69,35 +84,32 @@ public class LongCommonSubsequence {
      * @return
      */
     private static int lscDynamic(List<Character> a, List<Character> b) {
-        for (int i = 0; i < a.size(); i++) {
-            for (int j = 0; j < b.size(); j++) {
-
+        int[][] dp = new int[a.size() + 1][b.size() + 1];
+        for (int i = 1; i <= a.size(); i++) {
+            for (int j = 1; j <= b.size(); j++) {
+                if (a.get(i - 1).equals(b.get(j - 1))) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
             }
         }
-        return 0;
+        System.out.println("------------lscDynamic------------");
+        print2DArray(dp);
+        return dp[a.size()][b.size()];
     }
-
-    private static List<Character> resultList = new ArrayList<>();
 
     /**
-     * 递归实现
-     * 主要依靠减而治之和分而治之
-     * 时间复杂度：当两父序列长度相等时 O(2^n)
+     * 格式化打印二维数组
      *
-     * @param a
-     * @param b
-     * @return
+     * @param memo
      */
-    private static int recursiveLongCommonSubseq(List<Character> a, int aSize, List<Character> b, int bSize) {
-        if (aSize < 1 || bSize < 1) {
-            return 0;
-        }
-        if (a.get(aSize - 1).equals(b.get(bSize - 1))) {
-            return 1 + recursiveLongCommonSubseq(a, aSize - 1, b, bSize - 1);
-        } else {
-            return Math.max(recursiveLongCommonSubseq(a, aSize - 1, b, bSize), recursiveLongCommonSubseq(a, aSize, b, bSize - 1));
+    private static void print2DArray(int[][] memo) {
+        for (int[] row : memo) {
+            for (int i = 0; i < row.length; i++) {
+                System.out.print(row[i] == -1 ? "-1 " : " " + row[i] + " ");
+            }
+            System.out.println();
         }
     }
-
-
 }
